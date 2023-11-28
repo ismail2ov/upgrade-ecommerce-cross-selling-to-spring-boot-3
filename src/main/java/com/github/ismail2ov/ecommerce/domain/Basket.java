08996f481241d09740f3470c70b42d28b58a1d78
Ismail Ahmedov <ismail.ahmedov@asesormasmovil.es>
2023-11-28 14:10:30 +0100

    Migration from javax to jakarta namespace

--- src/main/java/com/github/ismail2ov/ecommerce/domain/Basket.java
+++ src/main/java/com/github/ismail2ov/ecommerce/domain/Basket.java
@@ -1,12 +1,12 @@
 package com.github.ismail2ov.ecommerce.domain;
 
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
