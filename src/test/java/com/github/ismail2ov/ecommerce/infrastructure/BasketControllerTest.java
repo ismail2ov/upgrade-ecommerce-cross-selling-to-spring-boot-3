1fcd52f002a6ad6238cb0b4f0746e985bb36a70c
Ismail Ahmedov <ismail.ahmedov@asesormasmovil.es>
2023-11-28 14:49:42 +0100

    Run OpenRewrite

--- src/test/java/com/github/ismail2ov/ecommerce/infrastructure/BasketControllerTest.java
+++ src/test/java/com/github/ismail2ov/ecommerce/infrastructure/BasketControllerTest.java
@@ -16,17 +16,14 @@ import com.github.ismail2ov.ecommerce.infrastructure.controller.mapper.BasketMap
 import com.github.ismail2ov.ecommerce.infrastructure.controller.mapper.ProductMapperImpl;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-@ExtendWith(SpringExtension.class)
 @WebMvcTest
 @ContextConfiguration(classes = BasketController.class)
 @Import({ProductMapperImpl.class, BasketMapperImpl.class})
--- src/test/java/com/github/ismail2ov/ecommerce/infrastructure/ProductControllerTest.java
+++ src/test/java/com/github/ismail2ov/ecommerce/infrastructure/ProductControllerTest.java
@@ -14,16 +14,13 @@ import com.github.ismail2ov.ecommerce.infrastructure.controller.mapper.ProductMa
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-@ExtendWith(SpringExtension.class)
 @WebMvcTest
 @ContextConfiguration(classes = ProductController.class)
 @Import(ProductMapperImpl.class)
